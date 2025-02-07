da43e2892ee7c0283ae3381744766d193b4e90d9
Lei <lei.xiao@UTSouthwestern.edu>
2025-02-07 16:56:25 -0600

    import namespace

--- R/spectrogramming.R
+++ R/spectrogramming.R
@@ -64,6 +64,8 @@
 #'                end_time_in_second = rep(5, 6))
 #' }
 #'
+#' @importFrom tuneR readWave
+#'
 #' @rdname visualize_song
 #' @export
 visualize_song <- function(x, ...) {
@@ -324,6 +326,8 @@ visualize_song.Sap <- function(x,  # sap object
 #' }
 #'
 #' @seealso \code{\link{visualize_song}} for single file visualization
+#'
+#' @importFrom tuneR readWave
 #'
 #' @rdname visualize_segments
 #' @export
